120c980a066c7abac7c72d82d3aa732cfff4da6d
Minh Khoi <Minhkhoi01102022@outlook.com.vn>
2024-07-14 21:45:24 +0700

    v1.0

---
+++ My2DGame/src/main/CollisionChecker.java
@@ -0,0 +1,180 @@
+package main;
+
+import entity.Entity;
+
+public class CollisionChecker {
+	
+	GamePanel gp;
+	
+	public CollisionChecker (GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	public void checkTile(Entity e) {
+		
+		int entityLeftWorldX = e.worldX + e.solidArea.x;
+		int entityRightWorldX = e.worldX + e.solidArea.x + e.solidArea.width;
+		int entityTopWorldY = e.worldY + e.solidArea.y;
+		int entityBotWorldY = e.worldY + e.solidArea.y + e.solidArea.height;
+		
+		int entityLeftCol = entityLeftWorldX/gp.tileSize;
+		int entityRightCol = entityRightWorldX/gp.tileSize;
+		int entityTopRow = entityTopWorldY/gp.tileSize;
+		int entityBotRow = entityBotWorldY/gp.tileSize;
+		
+		int tileNum1,tileNum2;
+		
+		switch(e.direction) {
+		case "up":
+			entityTopRow = (entityTopWorldY-e.speed)/gp.tileSize;
+			tileNum1 = gp.tileManager.mapTileNum[gp.currentMap][entityLeftCol][entityTopRow];
+			tileNum2 = gp.tileManager.mapTileNum[gp.currentMap][entityRightCol][entityTopRow];
+			if(gp.tileManager.tile[tileNum1].collision == true || gp.tileManager.tile[tileNum2].collision == true) {
+				e.collisionOn = true;
+			}
+			break;
+		case "down":
+			entityBotRow = (entityBotWorldY+e.speed)/gp.tileSize;
+			tileNum1 = gp.tileManager.mapTileNum[gp.currentMap][entityLeftCol][entityBotRow];
+			tileNum2 = gp.tileManager.mapTileNum[gp.currentMap][entityRightCol][entityBotRow];
+			if(gp.tileManager.tile[tileNum1].collision == true || gp.tileManager.tile[tileNum2].collision == true) {
+				e.collisionOn = true;
+			}
+			break;
+		case "left":
+			entityLeftCol = (entityLeftWorldX-e.speed)/gp.tileSize;
+			tileNum1 = gp.tileManager.mapTileNum[gp.currentMap][entityLeftCol][entityTopRow];
+			tileNum2 = gp.tileManager.mapTileNum[gp.currentMap][entityLeftCol][entityBotRow];
+			if(gp.tileManager.tile[tileNum1].collision == true || gp.tileManager.tile[tileNum2].collision == true) {
+				e.collisionOn = true;
+			}
+			break;
+		case "right":
+			entityRightCol = (entityRightWorldX+e.speed)/gp.tileSize;
+			tileNum1 = gp.tileManager.mapTileNum[gp.currentMap][entityRightCol][entityTopRow];
+			tileNum2 = gp.tileManager.mapTileNum[gp.currentMap][entityRightCol][entityBotRow];
+			if(gp.tileManager.tile[tileNum1].collision == true || gp.tileManager.tile[tileNum2].collision == true) {
+				e.collisionOn = true;
+			}
+			break;
+		}
+	}
+	
+	public int checkObject(Entity e, boolean player) {
+		
+		int index = 999;
+		
+		for(int i = 0; i < gp.obj[1].length; i++) {
+			
+			if(gp.obj[gp.currentMap][i] != null) {
+				
+				//Get entity's solid area position
+				e.solidArea.x = e.worldX + e.solidArea.x;
+				e.solidArea.y = e.worldY + e.solidArea.y;
+				
+				//Get object's solid area position
+				gp.obj[gp.currentMap][i].solidArea.x = gp.obj[gp.currentMap][i].worldX + gp.obj[gp.currentMap][i].solidArea.x;
+				gp.obj[gp.currentMap][i].solidArea.y = gp.obj[gp.currentMap][i].worldY + gp.obj[gp.currentMap][i].solidArea.y;
+				
+				switch(e.direction) {
+				case "up": e.solidArea.y -= e.speed; break;
+				case "down": e.solidArea.y += e.speed; break;
+				case "left": e.solidArea.x -= e.speed; break;
+				case "right": e.solidArea.x += e.speed; break;
+				}
+				
+				if(e.solidArea.intersects(gp.obj[gp.currentMap][i].solidArea)) {
+					if(gp.obj[gp.currentMap][i].collision == true) {
+						e.collisionOn = true;
+					}
+					if(player == true) {
+						index = i;
+					}
+				}
+				
+				e.solidArea.x = e.solidAreaDefaultX;
+				e.solidArea.y = e.solidAreaDefaultY;
+				gp.obj[gp.currentMap][i].solidArea.x = gp.obj[gp.currentMap][i].solidAreaDefaultX;
+				gp.obj[gp.currentMap][i].solidArea.y = gp.obj[gp.currentMap][i].solidAreaDefaultY;
+			}			
+		}
+		
+		return index;
+	}
+	
+	//NPC or monster
+	public int checkEntity(Entity e, Entity[][] target) {
+		
+		int index = 999;
+		
+		for(int i = 0; i < target[1].length; i++) {
+			
+			if(target[gp.currentMap][i] != null) {
+				
+				//Get entity's solid area position
+				e.solidArea.x = e.worldX + e.solidArea.x;
+				e.solidArea.y = e.worldY + e.solidArea.y;
+				
+				//Get object's solid area position
+				target[gp.currentMap][i].solidArea.x = target[gp.currentMap][i].worldX + target[gp.currentMap][i].solidArea.x;
+				target[gp.currentMap][i].solidArea.y = target[gp.currentMap][i].worldY + target[gp.currentMap][i].solidArea.y;
+				
+				switch(e.direction) {
+				case "up": e.solidArea.y -= e.speed; break;
+				case "down": e.solidArea.y += e.speed; break;
+				case "left": e.solidArea.x -= e.speed; break;
+				case "right": e.solidArea.x += e.speed; break;
+				}
+				
+				if(e.solidArea.intersects(target[gp.currentMap][i].solidArea)) {
+					
+					if(target[gp.currentMap][i] != e) { //If intersect target = entity, collision doesn't happen (avoid this entity include itself as a collision target
+						e.collisionOn = true;
+						index = i;
+					}	
+				}
+				
+				e.solidArea.x = e.solidAreaDefaultX;
+				e.solidArea.y = e.solidAreaDefaultY;
+				target[gp.currentMap][i].solidArea.x = target[gp.currentMap][i].solidAreaDefaultX;
+				target[gp.currentMap][i].solidArea.y = target[gp.currentMap][i].solidAreaDefaultY;
+			}			
+		}
+		
+		return index;
+		
+	}
+	
+	public boolean checkPlayer(Entity e) {
+		
+		boolean contactPlayer = false;
+		
+		//Get entity's solid area position
+		e.solidArea.x = e.worldX + e.solidArea.x;
+		e.solidArea.y = e.worldY + e.solidArea.y;
+		
+		//Get object's solid area position
+		gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
+		gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
+		
+		switch(e.direction) {
+		case "up": e.solidArea.y -= e.speed; break;
+		case "down": e.solidArea.y += e.speed; break;
+		case "left": e.solidArea.x -= e.speed; break;
+		case "right": e.solidArea.x += e.speed; break;
+		}
+		
+		if(e.solidArea.intersects(gp.player.solidArea)) {
+			e.collisionOn = true;
+			contactPlayer = true;
+		}
+		
+		e.solidArea.x = e.solidAreaDefaultX;
+		e.solidArea.y = e.solidAreaDefaultY;
+		gp.player.solidArea.x = gp.player.solidAreaDefaultX;
+		gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+		
+		return contactPlayer;
+	}
+	
+}
---
+++ My2DGame/src/main/GamePanel.java
@@ -0,0 +1,389 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import ai.PathFinder;
+import entity.Entity;
+import entity.Player;
+import environment.EnvironmentManager;
+import tile.TileManager;
+import tile_interactive.InteractiveTile;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	//screen settings
+	final int originalTileSize = 16; // 16*16 tile
+	final int scale = 3;
+	
+	public final int tileSize = originalTileSize * scale; // 48*48 tile
+	public final int maxScreenCol = 20;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; //960 pixels
+	public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	//World map settings
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int maxMap = 10;
+	public int currentMap = 0;
+	
+	//For full screen 
+	int screenWidth2 = screenWidth;
+	int screenHeight2 = screenHeight;
+	BufferedImage tempScreen;
+	Graphics2D g2;
+	public boolean fullScreenOn = false;
+	
+	//Frame per second
+	int FPS = 60;
+	
+	//System
+	public TileManager tileManager = new TileManager(this);
+	public KeyHandler keyHandler = new KeyHandler(this);
+	Sound music = new Sound();
+	Sound se = new Sound();
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
+	public UI ui = new UI(this);
+	public EventHandler	eHandler = new EventHandler(this);
+	Config config = new Config(this);
+	public PathFinder pFinder = new PathFinder(this);
+	public EnvironmentManager eManager = new EnvironmentManager(this);
+	Thread gameThread;
+	
+	//Entity and object
+	public Player player = new Player(this,keyHandler);
+	public Entity obj[][] = new Entity[maxMap][20];	
+	public Entity npc[][] = new Entity[maxMap][10];
+	public Entity monster[][] = new Entity[maxMap][20];
+	public InteractiveTile iTile[][] = new InteractiveTile[maxMap][50];
+	public Entity projectile[][] = new Entity[maxMap][20];
+//	public ArrayList<Entity> projectileList = new ArrayList<>();
+	public ArrayList<Entity>particleList = new ArrayList<>();
+	ArrayList<Entity> entityList = new ArrayList<>();
+	
+	//Game state
+	public int gameState;
+	public final int titleState = 0;
+	public final int playState = 1;
+	public final int pauseState = 2;
+	public final int dialogueState = 3;
+	public final int characterState = 4;
+	public final int optionsState = 5;
+	public final int gameOverState = 6;
+	public final int transitionState = 7;
+	public final int tradeState = 8;
+	public final int sleepState = 9;
+	
+	//Constructor
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyHandler);
+		this.setFocusable(true);
+	}
+	
+	public void setupGame() {
+		aSetter.setObject();
+		aSetter.setNPC();
+		aSetter.setMonster();
+		aSetter.setInteractiveTile();
+		eManager.setup();
+		
+		gameState = titleState;
+		
+		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+		g2 = (Graphics2D)tempScreen.getGraphics();
+		
+		if(fullScreenOn == true) {
+			setFullScreen();
+		}
+	}
+	
+	public void retry() {
+		
+		player.setDefaultPositions();
+		player.restoreLifeMana();
+		aSetter.setNPC();
+		aSetter.setMonster();
+	}
+	
+	public void restart() {
+		
+		player.setDefaultValues();
+		player.setDefaultPositions();
+		player.restoreLifeMana();
+		player.setItems();
+		aSetter.setObject();
+		aSetter.setNPC();
+		aSetter.setMonster();
+		aSetter.setInteractiveTile();
+	}
+	
+	public void setFullScreen() {
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		double width = screenSize.getWidth();
+
+		double height = screenSize.getHeight();
+
+		Main.window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		screenWidth2 = (int) width;
+
+		screenHeight2 = (int) height;
+	}
+	
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				//Update: update information such as character position
+				update();
+				//Draw: draw the screen with the update information
+				drawToTempScreen(); //Draw everything to the buffered image
+				drawToScreen(); // Draw buffered image to the screen
+				delta--;
+				drawCount++;
+			}
+			
+			if(timer >= 1000000000) {
+				System.out.println("FPS:"+drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+		}
+	}
+	
+	public void update() {
+		
+		if(gameState == playState) {
+			
+			//Player
+			player.update();
+			
+			//NPC
+			for(int i = 0; i < npc[1].length; i++) {
+				if(npc[currentMap][i] != null) {
+					npc[currentMap][i].update();
+				}
+			}
+			
+			//Monster
+			for(int i = 0; i < monster[1].length; i++) {
+				if(monster[currentMap][i] != null) {
+					if(monster[currentMap][i].alive == true && monster[currentMap][i].dying == false) {
+						monster[currentMap][i].update();
+					}
+					if(monster[currentMap][i].alive == false) {
+						monster[currentMap][i].checkDrop();
+						monster[currentMap][i] = null;
+					}
+				}
+			}
+			
+			for(int i = 0; i < projectile[1].length; i++) {
+				if(projectile[currentMap][i] != null) {
+					if(projectile[currentMap][i].alive == true) {
+						projectile[currentMap][i].update();
+					}
+					if(projectile[currentMap][i].alive == false) {
+						projectile[currentMap][i] = null;
+					}
+				}
+			}
+			
+			for(int i = 0; i < particleList.size(); i++) {
+				if(particleList.get(i) != null) {
+					if(particleList.get(i).alive == true) {
+						particleList.get(i).update();
+					}
+					if(particleList.get(i).alive == false) {
+						particleList.remove(i);
+					}
+				}
+			}
+			
+			for(int i = 0; i < iTile[1].length; i++) {
+				if(iTile[currentMap][i] != null) {
+					iTile[currentMap][i].update();
+				}
+			}
+			eManager.update();
+		}
+		
+		if(gameState == pauseState) {
+			
+		}
+	}
+	
+	public void drawToTempScreen() {
+		
+		//Debug
+		long drawStart = 0;
+		if(keyHandler.showDebugText == true) {
+			drawStart = System.nanoTime();
+		}
+				
+		//Title screen
+		if(gameState == titleState) {
+			ui.draw(g2);
+		}
+		//Others
+		else {
+					
+			//Tiles
+			tileManager.draw(g2);
+					
+			for(int i = 0; i < iTile[1].length; i++) {
+				if(iTile[currentMap][i] != null) {
+					iTile[currentMap][i].draw(g2);
+				}
+			}
+					
+			//Add entities to the list
+			entityList.add(player);
+					
+			for(int i = 0; i < npc[1].length; i++) {
+						
+				if(npc[currentMap][i] != null) {
+					entityList.add(npc[currentMap][i]);
+				}
+			}
+					
+			for(int i = 0; i < obj[1].length; i++) {
+						
+				if(obj[currentMap][i] != null) {
+					entityList.add(obj[currentMap][i]);
+				}
+			}
+					
+			for(int i = 0; i < monster[1].length; i++) {
+						
+				if(monster[currentMap][i] != null) {
+						entityList.add(monster[currentMap][i]);
+				}
+			}
+					
+			for(int i = 0; i < projectile[1].length; i++) {
+						
+				if(projectile[currentMap][i] != null) {
+					entityList.add(projectile[currentMap][i]);
+				}
+			}
+					
+			for(int i = 0; i < particleList.size(); i++) {
+						
+				if(particleList.get(i) != null) {
+					entityList.add(particleList.get(i));
+				}
+			}
+					
+			//Sort
+			Collections.sort(entityList, new Comparator<Entity>() {
+
+			@Override
+			public int compare(Entity e1, Entity e2) {
+							
+				int result = Integer.compare(e1.worldY, e2.worldY);
+				return result;
+			}
+						
+			});
+					
+			//Draw entities
+			for(int i = 0; i < entityList.size(); i++){
+				entityList.get(i).draw(g2);
+			}
+					
+			//Empty entity list
+			entityList.clear();
+					
+			//Environment
+			eManager.draw(g2);
+			
+			//UI
+			ui.draw(g2);				
+		}
+				
+		//Debug
+		if(keyHandler.showDebugText == true) {
+			long drawEnd = System.nanoTime();
+			long timePassed = drawEnd - drawStart;
+					
+			g2.setFont(new Font("Arial",Font.PLAIN,20));
+			g2.setColor(Color.white);
+			int x = 10;
+			int y = 400;
+			int lineHeight = 20;
+					
+			g2.drawString("WorldX" + player.worldX, x, y); y += lineHeight;
+			g2.drawString("WorldY" + player.worldY, x, y); y += lineHeight;
+			g2.drawString("Col" + (player.worldX + player.solidArea.x)/tileSize, x, y); y += lineHeight;
+			g2.drawString("Row" + (player.worldY + player.solidArea.y)/tileSize, x, y); y += lineHeight;
+					
+			g2.drawString("Draw time: "+ timePassed, x, y);
+		}
+	}
+	
+	public void drawToScreen() {
+		
+		Graphics g = getGraphics();
+		g.drawImage(tempScreen, 0, 0, screenWidth2, screenHeight2, null);
+		g.dispose();
+	}
+	
+	public void playMusic(int i) {
+		
+		music.setFile(i);
+		music.play();
+		music.loop();
+	}
+	
+	public void stopMusic() {
+		music.stop();
+	}
+	
+	public void playSE(int i) {
+		
+		se.setFile(i);
+		se.play();
+	}
+}
